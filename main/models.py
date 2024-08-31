481ccc7dfe8f8bfdeb6ad8a85641b5312250548a
Mauricio Cuello <mdavidcuello@gmail.com>
2024-08-31 17:24:24 +0200

    Pelican website initial commit

--- main/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class Comments(models.Model):
-    text = models.TextField()
-    name = models.CharField(max_length=70)
-    pub_date = models.DateTimeField(auto_now_add=True)
---
+++ pelicanconf.py
@@ -0,0 +1,63 @@
+AUTHOR = 'Mauricio Cuello'
+SITENAME = 'Tech&Coffee'
+SITEURL = ""
+
+PATH = "content"
+
+STATIC_PATHS = ["extra/custom.css","images"]
+
+EXTRA_PATH_METADATA = {
+    "extra/custom.css": {"path": "static/custom.css"},
+}
+
+MAIN_MENU = True
+
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
+
+# Default theme language.
+I18N_TEMPLATES_LANG = "en"
+
+
+
+# Pelican-search Configuration
+STORK_INPUT_OPTIONS = {"stemming": "German", "url_prefix": SITEURL}
+
+TIMEZONE = 'Europe/Madrid'
+
+DEFAULT_LANG = 'en'
+THEME = "Flex"
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Categories","/categories.html"),
+)
+
+# Social widget
+SOCIAL = (
+    ("github", "https://github.com/MauricioD13"),
+    ("linkedin", "https://www.linkedin.com/in/mauricio-cuello-a1369b1b5/"),
+    ("medium", "https://medium.com/@mdavidcuello"),
+    
+)
+
+MENUITEMS = {
+    ("Archives","/archives.html"),
+    ("Tags","/tags.html"),
+}
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
+
+USE_LESS = True
